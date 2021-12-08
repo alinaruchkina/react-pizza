7ac9aa77f44796878a450be8c78844178aec396d
alinaruchkina <alina.kartyschova@gmail.com>
2021-12-08 13:25:29 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from './Button';
+import Header from './Header';
+// import Categories from './Categories';
+import Categories from './Categories.jsx';
+
+export default { Button, Header, Categories };
