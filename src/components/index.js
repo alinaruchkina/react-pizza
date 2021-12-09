9274ff97b7d10d2cf2b65695e247669afc610a0b
alinaruchkina <alina.kartyschova@gmail.com>
2021-12-09 19:37:30 +0300

    added SortPopup

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
-import Button from './Button';
-import Header from './Header';
-// import Categories from './Categories';
-import Categories from './Categories.jsx';
-
-export default { Button, Header, Categories };
+export { default as Button } from './Button';
+export { default as Header } from './Header';
+export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
